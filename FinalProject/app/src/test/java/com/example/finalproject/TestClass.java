09f5904b9c30fba3e05af2ce93b5ba3a88199ef1
Andrea Herscovich <ahers063@uottawa.ca>
2019-11-09 15:47:02 -0500

    JUnit Test

---
+++ FinalProject/app/src/test/java/com/example/finalproject/TestClass.java
@@ -0,0 +1,55 @@
+package com.example.finalproject;
+
+import com.example.model.Person;
+import com.example.model.WalkInClinic;
+
+import org.junit.Test;
+
+public class TestClass {
+
+
+    WalkInClinic ClinicExample = new WalkInClinic(ClinicExample);
+
+    Person p1 = new Person("John", "password", "john@gmail.com", "Employee" );
+    Person p2 = new Person ("Sawyer", "password1", "sawyer@gmail.com", "Patient" );
+
+        @Test
+        public void addServiceAsPatientTest() {
+            //Should not allow patients to add tests
+            ClinicExample.addService("CheckUp", "Patient");
+            ClinicExample.addService("Flu Shot", "Patient");
+        }
+
+        @Test
+        public void addServiceTest() {
+            //Adds three services for this clinic
+            ClinicExample.addService("CheckUp", "Employee");
+            ClinicExample.addService("Write Prescription", "Employee");
+            ClinicExample.addService("Flu Shot","Employee");
+        }
+
+        @Test
+        public void deleteServiceTest() {
+            //deletes the third service
+            ClinicExample.deleteService("Flu Shot");
+        }
+
+        @Test
+        public void deleteUnexistingServiceTest() {
+            //should not do anything, the service is not offered by this clinic
+            ClinicExample.deleteService("Referral");
+        }
+
+        @Test
+        public void emailFormattedTest() {
+            //checks that the email is in correct format
+            boolean flag = true;
+            if (!(p1.getEmail().contains("@")) || !(p1.getEmail().contains(".ca")) || !(p1.getEmail().contains(".com"))) {
+                flag = false;
+            }
+            if (!flag) {
+                System.out.println("Email format is not valid");
+            }
+        }
+}
+
