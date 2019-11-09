0697df27a84f5901777e3883ceb949e5fea81cf0
Olivia Borel <obore031@uottawa.ca>
2019-11-08 22:38:55 -0500

    Adding Services pages

--- FinalProject/app/src/main/java/com/example/finalproject/edit_service.java
+++ FinalProject/app/src/main/java/com/example/finalproject/edit_service.java
@@ -3,6 +3,8 @@ package com.example.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class edit_service extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class edit_service extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_service);
+
+        Spinner chooseRole = (Spinner) findViewById(R.id.chooseRole);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(edit_service.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.roles));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        chooseRole.setAdapter(myAdapter);
+
     }
 }
