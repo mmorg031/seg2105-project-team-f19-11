0affa7a659652e5d52ffe6cfe39b77fb444b52ec
Haider Ali Lakdawala <mmorg031@uottawa.ca>
2019-11-16 15:32:46 -0500

    Adding functionality to about clinic pages

--- FinalProject/app/src/main/java/com/example/finalproject/dev3/AboutClinic.java
+++ FinalProject/app/src/main/java/com/example/finalproject/dev3/AboutClinic.java
@@ -10,6 +10,14 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.finalproject.R;
+import com.example.model.Employee;
+import com.example.model.WalkInClinic;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AboutClinic extends AppCompatActivity {
     private ImageButton backButton; //goes back to menu page
@@ -17,6 +25,7 @@ public class AboutClinic extends AppCompatActivity {
     private TextView addressOfClinic; //shows the saved address associated with clinic
     private TextView phoneOfClinic; //shows the saved phone number associated with clinic
     private Button editAbout; //goes to new page to edit information
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +39,60 @@ public class AboutClinic extends AppCompatActivity {
                 openactivity_clinicAbout();
             }
         });
+
+        editAbout = findViewById(R.id.button);
+        editAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity_editClinicAbout();
+            }
+        });
+
+        nameOfClinic = findViewById(R.id.nameText);
+        addressOfClinic = findViewById(R.id.addressText);
+        phoneOfClinic = findViewById(R.id.phoneText);
+
+        String userID = mFirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference("users/"+userID);
+
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapShot : dataSnapshot.getChildren()) {
+                    Employee user = dataSnapshot.getValue(Employee.class);
+                    WalkInClinic clinic = user.getClinic();
+
+                    nameOfClinic.setText(clinic.getName());
+                    addressOfClinic.setText(clinic.getLocation());
+                    phoneOfClinic.setText(clinic.getPhone());
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+
+
+        });
+
+
+
+
     }
 
     public void openactivity_clinicAbout(){
         Intent intent = new Intent(this, ClinicAbout.class);
+        startActivity(intent);
+    }
+
+    public void openactivity_editClinicAbout(){
+        Intent intent = new Intent(this, EditAboutClinic.class);
+        intent.putExtra("clinicName", nameOfClinic.getText());
+        intent.putExtra("clinicAddress", addressOfClinic.getText());
+        intent.putExtra("clinicPhone", phoneOfClinic.getText());
         startActivity(intent);
     }
 }
--- FinalProject/app/src/main/java/com/example/finalproject/dev3/EditAboutClinic.java
+++ FinalProject/app/src/main/java/com/example/finalproject/dev3/EditAboutClinic.java
@@ -2,9 +2,12 @@ package com.example.finalproject.dev3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.finalproject.R;
 
@@ -20,5 +23,38 @@ public class EditAboutClinic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_about_clinic);
+
+        Bundle extras = getIntent().getExtras();
+
+        backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity_aboutClinic();
+            }
+        });
+
+        saveButton = findViewById(R.id.saveBtn);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveClinicInfo();
+            }
+        });
+
+        nameOfClinic =  findViewById(R.id.writeName);
+        addressOfClinic = findViewById(R.id.writeAddress);
+        phoneOfClinic = findViewById(R.id.writePhone);
+    }
+
+
+    public void openactivity_aboutClinic(){
+        Intent intent = new Intent(this, AboutClinic.class);
+        startActivity(intent);
+    }
+
+    public void saveClinicInfo(){
+        //save and open about
+        openactivity_aboutClinic();
     }
 }
