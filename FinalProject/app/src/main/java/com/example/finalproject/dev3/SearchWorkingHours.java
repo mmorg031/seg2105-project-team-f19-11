a0e05bd64bce5f586cb09bf7b055f03d966644a3
Sawyer Murphy <smurp091@uottawa.ca>
2019-11-23 21:28:30 -0500

    Adding New Dev. 4 Pages

---
+++ FinalProject/app/src/main/java/com/example/finalproject/dev3/SearchWorkingHours.java
@@ -0,0 +1,16 @@
+package com.example.finalproject.dev3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalproject.R;
+
+public class SearchWorkingHours extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_working_hours);
+    }
+}
