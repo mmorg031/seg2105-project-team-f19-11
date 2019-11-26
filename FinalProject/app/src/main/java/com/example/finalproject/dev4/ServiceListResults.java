1cf69f47bc42ae69127ec8bf210ef3828eea3087
Sawyer Murphy <smurp091@uottawa.ca>
2019-11-26 03:10:12 -0500

    Fixing Page Connections

--- FinalProject/app/src/main/java/com/example/finalproject/dev4/ServiceListResults.java
+++ FinalProject/app/src/main/java/com/example/finalproject/dev4/ServiceListResults.java
@@ -2,15 +2,35 @@ package com.example.finalproject.dev4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.finalproject.R;
 
 public class ServiceListResults extends AppCompatActivity {
 
+    private ImageButton back ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_list_results);
+
+        back = (ImageButton) findViewById(R.id.backBtn) ;
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openactivity_bookingAppmnt();
+            }
+        });
+
+    }
+    public void openactivity_bookingAppmnt() {
+
+        Intent intent = new Intent(this, BookingAppmnt.class);
+        startActivity(intent);
     }
 }
